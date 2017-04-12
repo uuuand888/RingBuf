cecc59c47dc68322a57e0d3cb6bf4098fbf888ac
uuuand888 <uuu_888@qq.com>
2017-04-12 22:32:49 +0800

    v1
    第一个最简单的版本。

---
+++ RingBuffer.java
@@ -0,0 +1,36 @@
+/*
+ * 一个很简单的整数环形buffer。
+ * 
+ */
+
+
+public class RingBuffer {
+	public static final int MAX=32;
+	private int rbuf[];
+	private int head;
+	private int tail;
+	private int size;	
+	
+	RingBuffer(int size) {
+		this.size = size;
+		head = 0;
+		tail = 0;
+		rbuf = new int[size];
+	}
+	public int set(int a) {
+		if ((head-tail)>=size) {
+			return -1;
+		} else {
+			rbuf[head%size]=a;
+			head++;
+			return 0;
+		}
+	}
+	public int get() {
+		if (tail<head) {
+			return rbuf[(tail++)%size];
+		} else {
+			return -1;
+		}
+	}
+}
