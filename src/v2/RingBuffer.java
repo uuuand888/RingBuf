c5c111ab4d1e23a550ea2783831fa865b42981b3
uuuand888 <uuu_888@qq.com>
2017-04-13 22:03:17 +0800

    add v2

---
+++ src/v2/RingBuffer.java
@@ -0,0 +1,30 @@
+package src.v2;
+
+public class RingBuffer {
+	private int rbuf[];
+	private long head;
+	private long tail;
+	private long size;	
+	RingBuffer(long size) {
+		this.size = size;
+		head = 0;
+		tail = 0;
+		rbuf = new int[(int) size];
+	}
+	public int set(int a) {
+		if ((head-tail)==size) {
+			return -1;
+		} else {
+			rbuf[(int) (head&(size-1))]=a;
+			head++;
+			return 0;
+		}
+	}
+	public int get() {
+		if (head==tail)
+			return -1;
+		int ret = rbuf[(int) (tail&(size-1))];
+		tail++;
+		return ret;
+	}
+}
