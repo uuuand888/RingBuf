7ea19c4a402dff978d70ca2f5d054da6233f621d
uuuand888 <uuu_888@qq.com>
2017-05-21 21:59:58 +0800

    size to 2N.

--- src/v4/RingBuffer.java
+++ src/v4/RingBuffer.java
@@ -6,10 +6,25 @@ public class RingBuffer<T> {
 	private T[] rbuf;
 	private long head;
 	private long tail;
-	private int size;	
+	private int size;
+	
+	private int Find2N(int size) {
+		int	i=0;
+		int	n=1;
+		if (size<=4) {
+			n = 4;
+			return n;
+		}
+		while((size>>1)>0) {
+			size=size>>1;
+			i++;
+		}
+		return n<<(i+1);
+	}
+	@SuppressWarnings("unchecked")
 	public RingBuffer(Class<T> type, int size) {
-		this.size = size;
-		System.out.println(size);
+		this.size = Find2N(size);
+//		System.out.println("RingBuffer length is:"+this.size);
 		head = 0;
 		tail = 0;
 		rbuf = (T[]) Array.newInstance(type, size);  
--- test/RBTest.java
+++ test/RBTest.java
@@ -3,20 +3,8 @@ package test;
 import src.v4.*;
 
 public class RBTest {
-/*	public static void main(String argv[]) {
-		RingBuffer rb = new RingBuffer(16);
-		rb.set("11");
-		rb.set("aa");
-		rb.set("33");
-		rb.set("bb");
-		System.out.println(rb.get());
-		System.out.println(rb.get());
-		System.out.println(rb.get());
-		System.out.println(rb.get());
-		System.out.println(rb.get());
-	}*/
 	public static void main(String argv[]) {
-		RingBuffer<String> rb = new RingBuffer<String>(String.class, 16);
+		RingBuffer<String> rb = new RingBuffer<String>(String.class, 15);
 		rb.set("11");
 		rb.set("aa");
 		rb.set("33");
